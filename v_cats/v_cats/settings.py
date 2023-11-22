8dfa7bcd16283c3a36b07092a049164381dbec33
kirill <Nikishkin.KS@yandex.ru>
2023-11-22 20:12:10 +0300

    small edit

--- v_cats/v_cats/settings.py
+++ v_cats/v_cats/settings.py
@@ -92,7 +92,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('0.0.0.0', 6379)],
+            'hosts': [('redis', 6379)],
         },
     },
 }
